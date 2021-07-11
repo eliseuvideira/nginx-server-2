a6d114041b03731088fe72b214cb6bcb909775c1
Eliseu Videira <eliseuvideira1@gmail.com>
2021-07-11 16:19:15 -0300

    :sparkles: improved proxy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 const { Command } = require("commander");
+const { makeconfig } = require("./functions/makeconfig");
+const fs = require("fs-extra");
+const path = require("path");
 
 const VERSION = process.env.npm_package_version;
 
@@ -14,7 +17,15 @@ program
   .argument("email", "email to setup certbot\t\t\tex: eliseu@ev1.pw")
   .argument("location", "location to proxy to\t\t\tex: http://localhost:8080")
   .option("-s,--staging", "staging certbot", false)
-  .action((domain, email, location, options) => {
+  .action(async (domain, email, location, options) => {
+    const config = makeconfig(domain, location);
+
+    const configdir = path.join(__dirname, "..", "nginx", "domains");
+    await fs.mkdirp(configdir);
+
+    const configpath = path.join(configdir, domain + ".conf");
+    await fs.writeFile(configpath, config);
+
     console.log({ domain, email, location, options });
   })
   .showHelpAfterError();
